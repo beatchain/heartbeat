f16a2d3124dd8b16432907958d6eba86344d3005
carl-eriksen <carl.eriksen@beatchain.com>
2024-11-27 16:07:00 +0000

    add simple dev version

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const {createClient} = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseDevUrl = process.env.SUPABASE_DEV_URL;
 const supabasePrivateKey = process.env.SUPABASE_KEY;
-
+const supabaseDevPrivateKey = process.env.SUPABASE_DEV_KEY;
 const supabase = createClient(supabaseUrl, supabasePrivateKey);
+const supabaseDevClient = createClient(supabaseDevUrl, supabaseDevPrivateKey);
 
 async function repeatEveryThreeSeconds() {
-  const {error, count} = await supabase
-      .from('queue')
-      .select('*', {count: 'exact', head: true})
-      .eq('status', 'queued');
+  const { error, count } = await supabase
+    .from("queue")
+    .select("*", { count: "exact", head: true })
+    .eq("status", "queued");
 
   if (error) {
     console.error(error);
@@ -17,6 +19,19 @@ async function repeatEveryThreeSeconds() {
 
   if (count > 0) {
     await fetch(process.env.QUEUE_URL);
+  }
+  // do the same thing for dev
+  const { error: devError, count: devCount } = await supabaseDevClient
+    .from("queue")
+    .select("*", { count: "exact", head: true })
+    .eq("status", "queued");
+
+  if (devError) {
+    console.error(devError);
+  }
+
+  if (devCount > 0) {
+    await fetch(process.env.QUEUE_DEV_URL);
   }
 }
 
